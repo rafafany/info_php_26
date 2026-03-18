34d868410ee3a967e7b396b7d1676c5be30ee861
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-03-17 21:38:36 -0300

    exercicios de variavei, operadosres aritmeticos e de contenacao

--- variaveis.php
+++ variaveis.php
@@ -1,5 +1,54 @@
 <?php
+ //mock
+//r dados de usuario, sendo eles: 
+ 
+$nome = "rafa";
+$idade = "21";
+$sexo = "feminino";
+$nomePai = "jose guimaraes";
+$nomeMae ="maria lopes";
 
-$numero = 10;
+//*echo "o usuario de nome $nome " . "idade $idade". "sexo $sexo". "é filho de $nomePai". "e $nomeMae";
+//echo "<br>";
+//echo "o usuario de nome $nome idade $idade sexo $sexo é filho de $nomePai e $nomeMae";
 
-echo $numero; 
+$valor1 = "1";
+$valor2 = "1";
+$soma = $valor1 + $valor2;
+
+echo "a soma de $valor1 e $valor2 eh $soma";
+echo "<br>";
+$valor1 = "1";
+$valor2 = "1";
+$sub = $valor1 - $valor2;
+
+echo "a subtracao de $valor1 e $valor2 eh $sub"; 
+
+ echo "<br>";
+$valor1 = "1";
+$valor2 = "1";
+$mult = $valor1 * $valor2;
+
+echo "a multiplicaçao de $valor1 e $valor2 eh $mult"; 
+
+ echo "<br>";
+$valor1 = "1";
+$valor2 = "1";
+$div = $valor1 / $valor2;
+
+echo "a divisao de $valor1 e $valor2 eh $div"; 
+//$numero = 10;//int
+//$numero = 10.4;//float, long
+//$numero = "10";// string
+//$numero = "10"// char
+
+
+// tipos de estruturas
+// array || vetor
+
+//$array = [];
+//$array = array ();
+//$objeto = new stdClass (); // objeto ouu classe
+
+//echo $numero; 
+//echo date ("H:1:s");//
