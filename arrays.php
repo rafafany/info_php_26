6007a4284ea759d781eece1456a9684afa23fdec
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-05-05 22:00:25 -0300

    revisar

---
+++ arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/*array*/
+
+/* tipos de dados estruturais: array || vetor, objetos */ 
+
+$listaValores = [];
+
+
+print_r ($listaValores);
+
+$vetor1 = [1, 2, 3];
+$vetor2 = [1, 2, 3]; 
+$Rvetor = [1, 2, 3];
