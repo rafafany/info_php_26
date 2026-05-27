6f58582fce63df5b256e88b156966dee86df99d7
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-05-26 21:47:07 -0300

    revisar

--- Endereco.php
+++ Endereco.php
@@ -1,5 +1,14 @@
 <?php
 
-class Edenreco {
-    
+abstract class Endereco {
+    protected $rua;
+    protected $bairro;
+    protected $cep; 
+    protected $numero; 
+
+    public function __construct($cep, $bairro, $rua, $numero)
+    {
+
+    }
+ 
 }
--- Pessoa.php
+++ Pessoa.php
@@ -4,7 +4,7 @@ abstract class Pessoa {
     protected $nome;
     protected $idade;
     protected $telefone; 
-    protected endereco $endereco; 
+    protected Endereco $endereco; 
  
     public function __construct($nome, $idade, $telefone, $endereço) {
        $this ->nome = $nome;
